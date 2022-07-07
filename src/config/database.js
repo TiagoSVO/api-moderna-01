e141d37f34187eecd91d611c075d22cc198fbfe1
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2022-07-07 15:58:58 -0300

    [UPDATE] Criado e configurado o sequelize
    Foram instalados os pacotes:
    - sequelize
    - sequelize-cli

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "cstic",
+  password: "cstic",
+  database: "api-moderno-01",
+  define: {
+    timestamp: true,
+    underscore: true,
+    underscoreAll: true,
+  },
+};
