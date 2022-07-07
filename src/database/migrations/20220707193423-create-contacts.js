f0ef0ceb40fca3a45f66614229771dd6a4fff430
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2022-07-07 17:58:50 -0300

    [FEATURE] Created models and migrates in postgresql

---
+++ src/database/migrations/20220707193423-create-contacts.js
@@ -0,0 +1,42 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("contacts", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      customer_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "customers", key: "id" },
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("contacts");
+  },
+};
