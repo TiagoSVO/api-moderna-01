e0f076e15f0be3625551476296901f3d3dabe3b1
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-07-12 15:27:41 -0300

    [UPDATE] Created the filters to the query select in playground

--- src/playground.js
+++ src/playground.js
@@ -1,10 +1,27 @@
+import { Op } from "sequelize";
+
 import "./database";
 
 import Customer from "./app/models/Customer";
 
 class Playground {
   static async play() {
-    const customers = await Customer.findAll();
+    const customers = await Customer.findAll({
+      attributes: { exclude: ["id", "status"] },
+      where: {
+        [Op.or]: {
+          status: {
+            [Op.in]: ["ACTIVE", "ARCHIEVED"],
+          },
+          name: {
+            [Op.like]: "Ti%",
+          },
+        },
+        createdAt: {
+          [Op.between]: [new Date(2022, 1, 1), new Date(2022, 12, 31)],
+        },
+      },
+    });
 
     console.log(JSON.stringify(customers, null, 2));
   }
