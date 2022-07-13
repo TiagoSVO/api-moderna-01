5d27bb014083685ffd802fba0570503f63d85c08
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-07-13 14:52:09 -0300

    [UPDATED] Inserted the create, update and destroy methods for customer

--- src/playground.js
+++ src/playground.js
@@ -37,30 +37,73 @@ class Playground {
       offset: 3 * 1 - 3, // limit * page - limit
     });
 
+    console.log(JSON.stringify(customers, null, 2));
+
     const customersMin = await Customer.min("createdAt", {
       where: { status: "ACTIVE" },
     });
+
+    console.log(`Min createdAt: ${JSON.stringify(customersMin, null, 2)}`);
 
     const customersMax = await Customer.max("createdAt", {
       where: { status: "ACTIVE" },
     });
 
+    console.log(`Max createdAt: ${JSON.stringify(customersMax, null, 2)}`);
+
     const customersCount = await Customer.count();
 
+    console.log(`Count registers: ${JSON.stringify(customersCount, null, 2)}`);
+
     const customerScope = await Customer.scope("active").findAll();
+
+    console.log(`Scoped query: ${JSON.stringify(customerScope, null, 2)}`);
 
     const customerScopeMethod = await Customer.scope({
       method: ["created", new Date(2022, 12, 12)],
     }).findAll();
 
-    console.log(JSON.stringify(customers, null, 2));
-    console.log(`Min createdAt: ${JSON.stringify(customersMin, null, 2)}`);
-    console.log(`Max createdAt: ${JSON.stringify(customersMax, null, 2)}`);
-    console.log(`Count registers: ${JSON.stringify(customersCount, null, 2)}`);
-    console.log(`Scoped query: ${JSON.stringify(customerScope, null, 2)}`);
     console.log(
       `Scopedmethod query: ${JSON.stringify(customerScopeMethod, null, 2)}`
     );
+
+    let customerCreate = await Customer.findAll({
+      where: {
+        email: {
+          [Op.like]: "admin@croscode.com",
+        },
+      },
+    });
+
+    if (!customerCreate) {
+      customerCreate = await Customer.create({
+        name: "CrossCode",
+        email: "admin@croscode.com",
+      });
+    }
+
+    console.log(
+      `customerCreate query: ${JSON.stringify(customerCreate, null, 2)}`
+    );
+
+    const customerToUpdate = await Customer.findByPk(4);
+    console.log(`Customer antes: ${JSON.stringify(customerToUpdate, null, 2)}`);
+
+    const statusCustomer =
+      customerToUpdate.status === "ACTIVE" ? "ARCHIEVED" : "ACTIVE";
+
+    const customerUpdated = await customerToUpdate.update({
+      status: statusCustomer,
+    });
+
+    console.log(`Customer depois: ${JSON.stringify(customerUpdated, null, 2)}`);
+
+    const customerToDelete = await Customer.create({
+      name: "Customer To Delete definitly",
+      email: "customer_to_delete@definitly.com",
+    });
+
+    customerToDelete.destroy();
   }
 }
 
